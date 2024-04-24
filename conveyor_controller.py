c3e0120871fc90db0142f95c39e61c926133c977
Fauthog <tky40660@gmail.com>
2024-04-24 18:23:08 +0700

    update with acceleration limiter

--- conveyor_controller.py
+++ conveyor_controller.py
@@ -9,13 +9,11 @@ class conveyor_controller():
     def __init__(self):
         self.active:bool = False
         self.reverse:bool = False
-        self.delay:float = 0.0
-        self.driver_single_step:bool = False
         self.driver_shutdown:bool = False
         self.current_position:int = 0
-        self.setpoint:int = 0
+        self.position_setpoint:int = 0
         self.closed_loop:bool = False
-
+        self.speed_setpoint:int = 0        
         # setup GPIO
         # Define GPIO pins
         self.STEP_PIN = 18
@@ -32,87 +30,105 @@ class conveyor_controller():
         # # GPIO.setup(LIMIT_SWITCH_PIN_RIGHT, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # Pull-up resistor
         
         # magic numbers start
+        self.steps_per_rev:int = 400
         self.length:float = 350.0
         self.steps:int = 1473
         self.x_min:int = 0
         self.x_max:int = self.steps
         self.mm_per_step:float = self.length / self.steps
-        print("mm_per_step", self.mm_per_step)
-        self.steps_per_rev:int = 400
-        # Calculate delay based on RPM
-        self.delay_low_speed:float = 60.0 / (self.steps_per_rev * 60)   
-        self.delay_medium_speed:float = 60.0 / (self.steps_per_rev * 250) 
-        self.delay_high_speed:float = 60.0 / (self.steps_per_rev * 600) 
-        self.delay_single_step:float = 60.0 / (self.steps_per_rev * 10) 
+        
+        # pre-defined limits. These can be changed
+        self.low_speed:int = 60
+        self.medium_speed:int = 250
+        self.high_speed:int = 600
+        self.max_acceleration_per_step = 250 # max 250 rpm/step 
         #  magic numbers end
 
         self.start_driver()
 
+
     def driver(self)->None:
         print("driver")
+        current_speed:int = 0
+        position_error:float = 0.0
+        speed_setpoint:int = 0
         while (True):
-            time.sleep(1)
             if self.driver_shutdown:
                 return
            
-            # read variables
             active = self.active
             if active:
-                reverse = self.reverse
-                delay = self.delay
-                single_step = self.driver_single_step
                 closed_loop = self.closed_loop
-                setpoint = self.setpoint
-                # set direction pin
-                GPIO.output(self.DIR_PIN, not reverse)
-                # send pulse
-                error = setpoint - self.current_position
-                lower_limit_lock:bool = (self.current_position == self.x_min) and (reverse or (error<0))
-                upper_limit_lock:bool = (self.current_position == self.x_max) and (not reverse or (error>0))
 
-                if active and not lower_limit_lock and not upper_limit_lock: 
-                    if not single_step and not closed_loop :
+                if closed_loop:
+                    position_setpoint = self.position_setpoint # position setpoint in pulse space
+                    position = self.current_position # current position in pulse space
+                    speed_setpoint = self.speed_setpoint # speed setpoint in rpm
+                    # simple controller
+                    position_error = position_setpoint - position                   
+                    error_abs = abs(position_error)
+                    
+                    if 100 <= error_abs <=200:
+                        speed_setpoint = self.medium_speed
+                    if error_abs <= 100:
+                        speed_setpoint = self.low_speed
+                    if position_error > 0:
+                            reverse = False
+                    if position_error < 0:
+                            reverse = True
+                            speed_setpoint = -speed_setpoint # if we need to move in reverse, we have to use a negative speed_setpoint
+                    # limit acceleration
+                    speed_error = speed_setpoint - current_speed
+                    if speed_error < -self.max_acceleration_per_step:
+                        limited_speed = current_speed - self.max_acceleration_per_step
+                    elif speed_error > self.max_acceleration_per_step:
+                        limited_speed = current_speed + self.max_acceleration_per_step
+                    else:
+                        limited_speed = current_speed + speed_error
+
+
+                    lower_limit_lock:bool = (self.current_position == self.x_min) and (limited_speed < 0)
+                    upper_limit_lock:bool = (self.current_position == self.x_max) and (limited_speed > 0)
+                    if not lower_limit_lock or upper_limit_lock:
+                        if limited_speed < 0:
+                            GPIO.output(self.DIR_PIN, GPIO.HIGH) # reverse
+                            delay:float = 60.0 / (self.steps_per_rev * abs(limited_speed))
+                            GPIO.output(self.STEP_PIN, GPIO.HIGH)
+                            time.sleep(delay)
+                            self.current_position -= 1
+                            GPIO.output(self.STEP_PIN, GPIO.LOW)
+                            time.sleep(delay)
+
+                        elif limited_speed > 0:
+                            GPIO.output(self.DIR_PIN, GPIO.LOW) # forward
+                            delay:float = 60.0 / (self.steps_per_rev * limited_speed)
+                            GPIO.output(self.STEP_PIN, GPIO.HIGH)
+                            time.sleep(delay)
+                            self.current_position +=1
+                            GPIO.output(self.STEP_PIN, GPIO.LOW)
+                            time.sleep(delay)
+                        
+                        current_speed = limited_speed
+
+                if not closed_loop:
+                        reverse = self.reverse
+                        speed_setpoint = self.speed_setpoint
+                        # we are continuously moving the belt in a given direction at a given speed 
+                        delay:float = 60.0 / (self.steps_per_rev * speed_setpoint)
+                        GPIO.output(self.DIR_PIN, not reverse)
                         GPIO.output(self.STEP_PIN, GPIO.HIGH)
                         time.sleep(delay)
                         GPIO.output(self.STEP_PIN, GPIO.LOW)
                         time.sleep(delay)
                         print("pulse")
-                    elif single_step: # send only one pulse to make singular step
-                        # delay_single_step is fixed and will not be changed during runtime
-                        GPIO.output(self.STEP_PIN, GPIO.HIGH)
-                        time.sleep(self.delay_single_step)
-                        GPIO.output(self.STEP_PIN, GPIO.LOW)
-                        time.sleep(self.delay_single_step)
-                        print("single_step")
-                        self.driver_single_step=False
-                        self.active=False
-                        self.closed_loop = False
-                    elif closed_loop and not single_step: 
-                        print("closed_loop ", error)                   
-                        if error != 0:
-                            error_abs = abs(error)
-                            if error_abs > 100:
-                                delay=self.delay_medium_speed
-                            if error_abs <= 100:
-                                delay=self.delay_low_speed
-                            if error >= 0:
-                                reverse = False
-                            if error < 0:
-                                reverse = True
-                            GPIO.output(self.STEP_PIN, GPIO.HIGH)
-                            time.sleep(delay)
-                            GPIO.output(self.STEP_PIN, GPIO.LOW)
-                            time.sleep(delay)
+                        if reverse:
+                            self.current_position -= 1                        
                         else:
-                            self.driver_single_step=False
-                            self.active=False
-                            self.closed_loop = False
-
-                    if reverse:
-                        self.current_position -= 1
-                    else:
-                        self.current_position +=1
-                
+                            self.current_position +=1
+                    
+            else:
+                current_speed = 0
+                position_error = 0.0
 
     
 
@@ -129,56 +145,96 @@ class conveyor_controller():
         return
 
     def move(self, reverse:bool, speed:int)->None:
+        self.stop()
         print("move", reverse, speed)
+        self.closed_loop = False
         self.reverse = reverse
         if speed == 1:
-            self.delay=self.delay_low_speed
+            self.speed_setpoint = self.low_speed
         if speed == 2:
-            self.delay=self.delay_medium_speed
+            self.speed_setpoint = self.medium_speed
         if speed == 3:
-            self.delay=self.delay_high_speed
+            self.speed_setpoint = self.high_speed
         self.active=True       
 
-    def move_raw(self, reverse:bool, delay:float)->None:
+    def move_raw(self, reverse:bool, speed:float)->None:
+        self.stop()
         print("move_raw")
+        self.closed_loop = False
         self.reverse=reverse
-        if delay > 0:
-            self.delay=delay
+        if speed > 0 and speed <= self.high_speed:
+            self.speed_setpoint = speed
         self.active=True 
-    
-    def goto(self, position:float)->None:
+
+    def goto(self, position:float,  speed:int)->None:
+        # posiiont as float value between 0 and self.length
         print("goto ", position)
+        # clamp position to within the limits
         if position < self.x_min:
             position = self.x_min
         if position > self.x_max:
             position = self.x_max
         if (position >= self.x_min) and (position <= self.x_max):
-            self.setpoint=position/self.mm_per_step
+            if speed == 1:
+                self.speed_setpoint = self.low_speed
+            if speed == 2:
+                self.speed_setpoint = self.medium_speed
+            if speed == 3:
+                self.speed_setpoint = self.high_speed
+            self.position_setpoint=position/self.mm_per_step
+            self.closed_loop=True
+            self.active=True
+
+    def goto_plus_x(self, x:float,  speed:int)->None:
+        # posiiont as float value between 0 and self.length
+        print("goto plus x ", x)
+        # clamp position to within the limits
+        position = self.get_current_position() + x
+        if position < self.x_min:
+            position = self.x_min
+        if position > self.x_max:
+            position = self.x_max
+        if (position >= self.x_min) and (position <= self.x_max):
+            if speed == 1:
+                self.speed_setpoint = self.low_speed
+            if speed == 2:
+                self.speed_setpoint = self.medium_speed
+            if speed == 3:
+                self.speed_setpoint = self.high_speed
+            self.position_setpoint=position/self.mm_per_step
+            self.closed_loop=True
+            self.active=True
+    
+    def goto_with_speed(self, position:float, speed:int)->None:
+        # posiiont as float value between 0 and self.length, speed in int rpm 
+        print("goto ", position)
+        # clamp position to within the limits
+        if position < self.x_min:
+            position = self.x_min
+        if position > self.x_max:
+            position = self.x_max
+        if (position >= self.x_min) and (position <= self.x_max):
+            self.speed_setpoint = 60.0 / (self.steps_per_rev * speed)
+            self.position_setpoint=position/self.mm_per_step
             self.closed_loop=True
             self.active=True
             
         
     def goto_min(self)->None:
         print("goto_min")
-        self.setpoint=self.x_min
-        self.closed_loop=True
-        self.active=True
+        self.goto(self.x_min, 2)
+       
 
     def goto_max(self)->None:
         print("goto_max")
-        self.setpoint=self.x_max
-        self.closed_loop=True
-        self.active=True
-    
-    def single_step(self, reverse:bool)->None:
-        print("single_step")
-        self.reverse = reverse
-        self.driver_single_step = True
-        self.active = True
+        self.goto(self.x_max, 2)
+
     
     def stop(self)->None:
         print("stop")
         self.active = False
+        self.position_setpoint=self.get_current_position
+        time.sleep(0.01)
 
     def get_current_position(self)->float:
         print("get_current_position")
@@ -191,92 +247,12 @@ class conveyor_controller():
         print("current_position", self.current_position)
     
       
-    # user input functions below
-    def user_input(self)->None:
-        self.lock:bool=False
-        self.last_key:str=""
-
-        with Listener(
-            on_press=self.on_press,
-            on_release=self.on_release) as listener:
-            listener.join()
-
-    def on_press(self, key):
-        # print(key)
-        if key == Key.esc:
-            self.stop()
-            self.shutdown_driver()           
-            return False
-        
-        if str(key) == "'z'":
-            self.zero()
-        
-        if str(key) == "'+'":
-            self.goto_max()
-
-        if str(key) == "'-'":
-            self.goto_min()
-
-        if str(key) == "'s'":
-            self.stop()
-        
-        if str(key) == "'g'":
-            i=input()
-            i.strip()
-            try:
-                g=int(i[-3:])            
-                self.goto(g)
-            except:
-                print("NaN")
-
-        if not self.lock:      
-            # single step         
-            if str(key) == "<103>":
-                self.single_step(False)
-                self.lock=True
-                self.last_key=key
-
-            if str(key) == "<105>":
-                self.single_step(True)
-                self.lock=True
-                self.last_key=key
-            
-            #  move fast                
-            if str(key) == "<100>":
-                self.move(False, 2)
-                self.lock=True
-                self.last_key=key
-
-            if str(key) == "<102>":
-                self.move(True, 2)
-                self.lock=True
-                self.last_key=key
-
-            #  move slow            
-            if str(key) == "<97>":
-                self.move(False, 1)
-                self.lock=True
-                self.last_key=key
-
-            if str(key) == "<99>":
-                self.move(True, 1)
-                self.lock=True
-                self.last_key=key
-           
-    def on_release(self, key):
-        if key==self.last_key:
-            self.lock=False
-            
-        if key == Key.esc:
-            self.stop()
-            self.shutdown_driver()  
-            # Stop listener
-            return False
+    
 
 
 def main()->None:   
     driver=conveyor_controller()
-    driver.user_input()
+    
     
 if __name__ == "__main__":
     main()
